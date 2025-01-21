fe446953880b734e4358319656edc8dd3db420cc
Mnunez1001 <mnunezpalomares@guilford.edu>
2025-01-20 18:16:04 -0600

    3 commit

--- src/main/java/com/example/PolynomialDriver.java
+++ src/main/java/com/example/PolynomialDriver.java
@@ -2,7 +2,8 @@ package com.example;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
+import java.util.Collections;
+import java.util.Random;
     
 
 public class PolynomialDriver {
@@ -13,7 +14,7 @@ public class PolynomialDriver {
 
         Polynomial p1 = new Polynomial();
         System.out.println("Test 1: Empty Constructor");
-        System.out.println("p1 = " + p1);
+        System.out.println("Empty Polynomial p1 = " + p1);
         System.out.println();
 
         // Test 2: Constructure with Coefficients
@@ -21,7 +22,7 @@ public class PolynomialDriver {
         ArrayList<Double> coefficients = new ArrayList<>(Arrays.asList(1.0, 2.0, 3.0, 4.0));
         Polynomial p2 = new Polynomial(coefficients);
         System.out.println("Test 2: Constructor with Coefficients (ArrayList)");
-        System.out.println("p2 = " + p2);
+        System.out.println("Polynomial made with an ArrayList p2 = " + p2);
         System.out.println();
         
         // Test 3: Constructor with int array
@@ -29,19 +30,20 @@ public class PolynomialDriver {
         int[] coefficients2 = {1, 2, 3, 4};
         Polynomial p3 = new Polynomial(coefficients2);
         System.out.println("Test 3: Constructor with int array");
-        System.out.println("p3 = " + p3);
+        System.out.println("Polynomial made with an array p3 = " + p3);
         System.out.println();
 
         // Test 4: Random polynomial constructor base on degree input
 
         Polynomial p4 = new Polynomial(4); //Degree 4 with random coefficients
         System.out.println("Test 4: Random polynomial base on degree input");
-        System.out.println("p4 = " + p4);
+        System.out.println("Polynomial made with inputting a degree p4 = " + p4);
         System.out.println();
 
         // Test 5: Getters
 
         System.out.println("Test 5: Getters");
+        System.out.println("The polynomial p2 is: " + p2);
         System.out.println("Degree of p2:" + p2.getDegree());
         System.out.println("Coefficient of p2: " + Arrays.toString(p2.getCoefficients())); 
         System.out.println("Coefficient of x^2 in p2: " + p2.getCoefficient(2));
@@ -50,6 +52,7 @@ public class PolynomialDriver {
         // Test 6: Setters
 
         System.out.println("Test 6: Setters");
+        System.out.println("The original polynomial p2 is: " + p2);
         p2.setCoefficient(2, 5.0); //Change coefficient of x^2 to 5
         System.out.println("p2 after changing coefficient of x^2 to 5: " + p2);
         ArrayList<Double> newCoefficients = new ArrayList<>(Arrays.asList(1.0, 2.0, 3.0, 4.0, 5.0));
@@ -60,6 +63,7 @@ public class PolynomialDriver {
         // Test 7: Clear Method
 
         System.out.println("Test 7: Clear Method");
+        System.out.println("p2 before clear: " + p2);
         p2.clear();
         System.out.println("p2 after clear: " + p2);
         System.out.println();
@@ -78,6 +82,8 @@ public class PolynomialDriver {
 
         System.out.println("Test 9: Addition and Subtraction");
         Polynomial p5 = new Polynomial(new ArrayList<>(Arrays.asList(7.0, 8.0, 9.0, 10.0, 11.0)));
+        System.out.println("Polynomial p5 = " + p5);
+        System.out.println("Polynomial p2 = " + p2);
         Polynomial sum = p5.add(p2);
         Polynomial difference = p5.subtract(p2);
         System.out.println("p5 + p2 = " + sum);
@@ -87,14 +93,51 @@ public class PolynomialDriver {
         //Test 10: CompareTo Method
 
         System.out.println("Test 10: CompareTo Method");
+        System.out.println("Polynomial p5 = " + p5);
+        System.out.println("Polynomial p2 = " + p2);
         System.out.println("Comparing p2 and p5: " + p2.compareTo(p5));
         System.out.println("Comparing p5 and p2: " + p5.compareTo(p2));
         Polynomial p6 = new Polynomial(newCoefficients);
+        System.out.println("Polynomial p6 = " + p6);
         System.out.println("Comparing p2 and p6: " + p2.compareTo(p6));
-        System.out.println();   
-
-        System.out.println("----- End of Testing Polynomial Class -----");
+        System.out.println();  
         
+        
+
+        //Test 11: List of Random Polynomials 
+
+        System.out.println("Test 11: List of Random Sorted Polynomials");
+
+        ArrayList<Polynomial> polynomials = new ArrayList<>();
+
+        for (int i = 0; i < 5; i++){
+            Random rand = new Random();
+            //Generate degree between 1 and 15
+            int degree = rand.nextInt(15) + 1;
+            Polynomial p = new Polynomial(degree);
+            polynomials.add(p);
+        }
+
+
+
+        System.out.println("\nList of unsorted polynomials:");
+        for (Polynomial p : polynomials){
+            System.out.println("\n" + p);
+        }
+
+        Collections.sort(polynomials);
+
+        System.out.println("\nList of sorted polynomials:");
+        for (Polynomial p : polynomials){
+            System.out.println("\n" + p);
+        }
+
+
+
+
+
+        System.out.println("\n----- End of Testing Polynomial Class -----");
+
 
 
 
